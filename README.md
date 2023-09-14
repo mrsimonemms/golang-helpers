986aaf5d2782867c3c8c4a2c446cc1bcd075b0b2
Simon Emms <simon@simonemms.com>
2023-09-14 20:02:20 +0000

    feat(logger): create golang logger helper

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Common Golang helpers
 
 <!-- toc -->
 
+* [Logger](#logger)
 * [Contributing](#contributing)
   * [Open in Gitpod](#open-in-gitpod)
   * [Open in a container](#open-in-a-container)
@@ -11,6 +12,28 @@ Common Golang helpers
 <!-- Regenerate with "pre-commit run -a markdown-toc" -->
 
 <!-- tocstop -->
+
+## Logger
+
+This is useful for getting a custom log level in a Cobra program.
+
+```go
+package cmd
+
+import "github.com/mrsimonemms/golang-helpers/logger"
+
+var logLevel string
+
+var root = &cobra.Command{
+  PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+    return logger.SetLevel(logLevel)
+  },
+}
+
+func init() {
+  rootCmd.PersistentFlags().StringVarP(&logLevel, "log-level", "l", logrus.InfoLevel.String(), fmt.Sprintf("log level: %s", logger.GetAllLevels()))
+}
+```
 
 ## Contributing
 
---
+++ logger/logger.go
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023 Simon Emms <simon@simonemms.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package logger
+
+import (
+	"slices"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Logger is default instance of logger used in all other packages
+// instead of global scope logrus.Logger.
+var Logger *logrus.Logger
+
+func init() {
+	Logger = logrus.New()
+}
+
+func GetAllLevels() string {
+	l := []string{}
+	for _, s := range logrus.AllLevels {
+		l = append(l, s.String())
+	}
+
+	slices.Reverse(l)
+	return strings.Join(l, ", ")
+}
+
+func SetLevel(logLevel string) error {
+	level, err := logrus.ParseLevel(logLevel)
+	if err != nil {
+		return err
+	}
+
+	Logger.SetLevel(level)
+	return nil
+}
+
+// Log is used to return the default Logger.
+func Log() *logrus.Logger {
+	return Logger
+}
