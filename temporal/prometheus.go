66e9a42b7ca2e1e8d2103991f1e3c3a074f3b24e
Simon Emms <simon@simonemms.com>
2025-08-28 09:50:43 +0100

    feat: add more temporal helpers

---
+++ temporal/prometheus.go
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023 Simon Emms <simon@simonemms.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package temporal
+
+import (
+	"fmt"
+	"time"
+
+	prom "github.com/prometheus/client_golang/prometheus"
+	"github.com/rs/zerolog/log"
+	"github.com/uber-go/tally/v4"
+	"github.com/uber-go/tally/v4/prometheus"
+	"go.temporal.io/sdk/client"
+	sdktally "go.temporal.io/sdk/contrib/tally"
+)
+
+func NewPrometheusHandler(listenAddress, prefix string) (client.MetricsHandler, error) {
+	c := prometheus.Configuration{
+		ListenAddress: listenAddress,
+		TimerType:     "histogram",
+	}
+
+	reporter, err := c.NewReporter(
+		prometheus.ConfigurationOptions{
+			Registry: prom.NewRegistry(),
+			OnError: func(err error) {
+				log.Fatal().Err(err).Msg("Error in Prometheus reporter")
+			},
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error creating reporter: %w", err)
+	}
+
+	scopeOpts := tally.ScopeOptions{
+		CachedReporter:  reporter,
+		Separator:       prometheus.DefaultSeparator,
+		SanitizeOptions: &sdktally.PrometheusSanitizeOptions,
+		Prefix:          prefix,
+	}
+	scope, _ := tally.NewRootScope(scopeOpts, time.Second)
+	scope = sdktally.NewPrometheusNamingScope(scope)
+
+	log.Info().Str("address", listenAddress).Msg("Starting Prometheus server")
+	return sdktally.NewMetricsHandler(scope), nil
+}
