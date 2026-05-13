d66d32594a11de267f07aa6cc8d0372bf088f9a5
Simon Emms <simon@simonemms.com>
2026-05-13 10:26:30 +0000

    feat: add cobra helpers for temporal
    Signed-off-by: Simon Emms <simon@simonemms.com>

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/samber/slog-zerolog/v2 v2.9.2
 	github.com/sirupsen/logrus v1.9.4
 	github.com/spf13/cobra v1.10.2
+	github.com/spf13/viper v1.21.0
 	github.com/stretchr/testify v1.11.1
 	github.com/uber-go/tally/v4 v4.1.17
 	go.temporal.io/api v1.62.11
@@ -34,6 +35,8 @@ require (
 	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/facebookgo/clock v0.0.0-20150410010913-600d898af40a // indirect
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -47,6 +50,7 @@ require (
 	github.com/muesli/termenv v0.16.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/nexus-rpc/sdk-go v0.6.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
@@ -54,14 +58,20 @@ require (
 	github.com/prometheus/procfs v0.20.1 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/robfig/cron v1.2.0 // indirect
+	github.com/sagikazarmark/locafero v0.11.0 // indirect
 	github.com/samber/lo v1.53.0 // indirect
 	github.com/samber/slog-common v0.22.0 // indirect
+	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
+	github.com/spf13/afero v1.15.0 // indirect
+	github.com/spf13/cast v1.10.0 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/stretchr/objx v0.5.3 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/twmb/murmur3 v1.1.8 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.yaml.in/yaml/v2 v2.4.4 // indirect
+	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/net v0.53.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.43.0 // indirect
---
+++ temporal/cobra.go
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2023 Simon Emms <simon@simonemms.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package temporal
+
+// This file contains helper functions to integrate Temporal options with Cobra and Viper for command-line applications.
+//
+// Example implementation:
+/**
+cmd := &cobra.Command{
+	Use:   "run",
+	Short: "Run a Temporal worker",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		c, err := temporal.NewConnection(append(
+			temporal.ParseCobraOpts(opts.temporal),
+			temporal.WithZerolog(&log.Logger),
+		)...)
+		if err != nil {
+			return gh.FatalError{
+				Cause: err,
+				Msg:   "Unable to create client",
+			}
+		}
+		defer c.Close()
+
+		w := worker.New(c, TaskQueue, worker.Options{})
+
+		// Start the healthcheck server in a separate goroutine
+		temporal.NewHealthCheck(cmd.Context(), []string{TaskQueue}, opts.temporal.HealthListenAddress, c)
+
+		if err := w.Run(worker.InterruptCh()); err != nil {
+			return gh.FatalError{
+				Cause: err,
+				Msg:   "Worker stopped",
+			}
+		}
+
+		return nil
+	},
+}
+*/
+
+import (
+	gh "github.com/mrsimonemms/golang-helpers"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"go.temporal.io/sdk/client"
+)
+
+type TemporalOpts struct {
+	Address              string
+	APIKey               string
+	HealthListenAddress  string
+	MetricsListenAddress string
+	MetricsPrefix        string
+	MTLSCertPath         string
+	MTLSKeyPath          string
+	Namespace            string
+	ServerName           string
+	TLSEnabled           bool
+}
+
+func NewCobraOpts(cmd *cobra.Command, opts *TemporalOpts) *TemporalOpts {
+	viper.SetDefault("health_listen_address", "0.0.0.0:3000")
+	cmd.Flags().StringVar(
+		&opts.HealthListenAddress, "health-listen-address",
+		viper.GetString("health_listen_address"), "Address of health server",
+	)
+
+	viper.SetDefault("metrics_listen_address", "0.0.0.0:9090")
+	cmd.Flags().StringVar(
+		&opts.MetricsListenAddress, "metrics-listen-address",
+		viper.GetString("metrics_listen_address"), "Address of Prometheus metrics server",
+	)
+
+	cmd.Flags().StringVar(
+		&opts.MetricsPrefix, "metrics-prefix",
+		viper.GetString("metrics_prefix"), "Prefix for metrics",
+	)
+
+	viper.SetDefault("temporal_address", client.DefaultHostPort)
+	cmd.Flags().StringVarP(
+		&opts.Address, "temporal-address", "H",
+		viper.GetString("temporal_address"), "Address of the Temporal server",
+	)
+
+	cmd.Flags().StringVar(
+		&opts.APIKey, "temporal-api-key",
+		viper.GetString("temporal_api_key"), "API key for Temporal authentication",
+	)
+	// Hide the default value to avoid spaffing the API to command line
+	gh.HideCommandOutput(cmd, "temporal-api-key")
+
+	cmd.Flags().StringVar(
+		&opts.MTLSCertPath, "tls-client-cert-path",
+		viper.GetString("temporal_tls_client_cert_path"), "Path to mTLS client cert, usually ending in .pem",
+	)
+
+	cmd.Flags().StringVar(
+		&opts.MTLSKeyPath, "tls-client-key-path",
+		viper.GetString("temporal_tls_client_key_path"), "Path to mTLS client key, usually ending in .key",
+	)
+
+	viper.SetDefault("temporal_namespace", client.DefaultNamespace)
+	cmd.Flags().StringVarP(
+		&opts.Namespace, "temporal-namespace", "n",
+		viper.GetString("temporal_namespace"), "Temporal namespace to use",
+	)
+
+	cmd.Flags().StringVar(
+		&opts.ServerName, "temporal-server-name",
+		viper.GetString("temporal_server_name"),
+		"Override the TLS server name (SNI) used for certificate validation. "+
+			"Required when the endpoint address does not match the certificate hostname, for example AWS PrivateLink.",
+	)
+
+	cmd.Flags().BoolVar(
+		&opts.TLSEnabled, "temporal-tls",
+		viper.GetBool("temporal_tls"), "Enable TLS Temporal connection",
+	)
+
+	return opts
+}
+
+func ParseCobraOpts(opts *TemporalOpts) []Options {
+	return []Options{
+		WithHostPort(opts.Address),
+		WithNamespace(opts.Namespace),
+		WithTLS(opts.TLSEnabled, WithTLSServerName(opts.ServerName)),
+		WithAuthDetection(
+			opts.APIKey,
+			opts.MTLSCertPath,
+			opts.MTLSKeyPath,
+		),
+		WithPrometheusMetrics(opts.MetricsListenAddress, opts.MetricsPrefix, nil),
+	}
+}
